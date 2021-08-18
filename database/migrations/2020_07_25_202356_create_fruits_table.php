9a42b76286643983a938549e7d9c9dcaf79a9e6e
unknown <vipinmathpal11@gmail.com>
2021-08-19 00:10:34 +0530

    'update'

--- database/migrations/2020_07_25_202356_create_fruits_table.php
+++ database/migrations/2020_07_25_202356_create_fruits_table.php
@@ -18,7 +18,7 @@ class CreateFruitsTable extends Migration
                  $table->string('name');
             $table->integer('price');
             $table->integer('stock');
-            $table->integer('link');
+            $table->string('link');
             $table->timestamps();
         });
     }
